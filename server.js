c90dee6e346bae7c5acaf2e4933dc530ed318b8c
Soumyy7 <136830871+Soumyy7@users.noreply.github.com>
2023-07-24 23:11:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,16 +13,17 @@ const app = express();
 // middleware
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
 });
 
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
 // routes
 app.use("/api/workouts", workoutRoutes);
 app.use("/api/user", userRoutes);
