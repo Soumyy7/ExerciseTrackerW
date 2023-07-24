084830c4aff27087bbfcf851435dc81069bddd78
Soumyy7 <136830871+Soumyy7@users.noreply.github.com>
2023-07-24 23:31:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,15 +18,15 @@ app.use((req, res, next) => {
   next();
 });
 
+// routes
+app.use("/api/workouts", workoutRoutes);
+app.use("/api/user", userRoutes);
+
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
 });
-
-// routes
-app.use("/api/workouts", workoutRoutes);
-app.use("/api/user", userRoutes);
 
 const port = process.env.PORT || 4000;
 // connect to db
